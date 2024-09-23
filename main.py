dfe73feac67c4215dda24561c79d8a4f3920ca21
Sree-Rangaraj89 <sreerangaraj1@gmail.com>
2024-09-22 17:36:51 -0700

    python weight age is student

---
+++ main.py
@@ -0,0 +1,20 @@
+name "rangaraj"
+age "12"
+is_student = true
+weight = 40.2
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data type of Age is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is ", type(age))
+weight = int(weight)
+print(weight)
